befa5e9d22cd9f752209d749cdd91a5f1a31f806
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-09-18 20:58:08 -0300

    feat: iniciando a subclasse cliente

---
+++ Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente extends Usuario {
+
+    public Cliente(String nome, String telefone, String email) {
+        super(nome, telefone, email);
+    }
+
+    public void agendar() {
+
+    }
+}
