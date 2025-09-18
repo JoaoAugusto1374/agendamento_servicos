30e9b3cc275132eb047b6319dab23cc6cad10029
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-09-18 20:57:39 -0300

    feat: criando a superclasse usuário

---
+++ Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    String nome;
+    String telefone;
+    String email;
+    
+    public Usuario(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        
+    }
+
+}
