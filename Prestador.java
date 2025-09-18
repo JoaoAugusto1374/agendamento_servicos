cb0f87fa91866c0ef5d06568559b8bff9d2bad89
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-09-18 20:59:58 -0300

    feat: iniciando a subclasse prestador

---
+++ Prestador.java
@@ -0,0 +1,15 @@
+public class Prestador extends Usuario{
+    Servico servico;
+
+    public Prestador(String nome, String telefone, String email) {
+        super(nome, telefone, email);
+    }
+
+    public void ofertarServicos() {
+
+    }
+
+    public void mostrarAgenda() {
+
+    }
+}
